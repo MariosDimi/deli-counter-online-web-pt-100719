5d054f6ab31de1f135e07e1af6cf9cd5c075fc19
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-13 18:05:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,7 @@
-katz_deli = []
-
-def line 
+katz_deli = ["Ada", "Grace", "Rivki"]
+  current_line = "The simple line is currently:"
+  array.each.with_index(1) do |value, indexemus|
+    current_line << "#{indexemus}. #{value},"
+  end
+  puts current_line
+end
