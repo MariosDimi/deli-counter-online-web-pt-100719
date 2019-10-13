6a1475d2b94304da5d5cd22f15f44f846c916adb
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-13 18:40:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,16 @@
 katz_deli = []
 
-def line_method(numinline)
-  line_method_array = []
-  if numinline.length == 0
-    puts "The line is currently empty."
-  else
-    line_method_array.each.with_index(0) do |name, index|
-      line_method_array.push("#{index}. #{name}")
-      end
-    puts "The line is: #{line_method_array.join(" ")}"
-  end
+ def line(katz_deli)
+ if katz_deli.empty? == false
+   katz_deli.each_with_index do|value,index|
+
+     puts "The line is currently: #{index+1}. #{value}"
+ end
+     elsif katz_deli.empty? == true
+  puts "The line is currently empty."
+ end
 end
+
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
