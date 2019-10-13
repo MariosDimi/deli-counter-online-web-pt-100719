a2e903e992d1e7e098e272229095db27cc5a8e31
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-13 18:39:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,3 +25,12 @@ def now_serving(nextinline)
     nextinline.shift
   end
 end
+
+line_method(katz_deli)
+take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Kent")
+line_method(katz_deli)
+now_serving(katz_deli)
+
+take_a_number(katz_deli, "Matz")
