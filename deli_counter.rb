044109a50a0ba2530376fcc99b4c580c3d0665d9
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-13 18:30:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,39 @@
-katz_deli = ["Ada", "Grace", "Rivki"]
+katz_deli = []
 
 def line(array)
-  current_line = "The line is currently:"
-  array.each.with_index(1) do |value, indexemus|
-    current_line << "#{indexemus}. #{value},"
-  end
-  puts current_line
-end
-
-line_simple(katz_deli)
-
-
-def take_number(line, new_customer)
-  line.push(new_customer)
-  puts "Welcome, #{new_customer}. You are number #{line_length} in line."
-end
-
-def now_serving(line)
-  if line.length == 0
-    puts "There is nobody waiting to be served!"
+  if array.length == 0
+    puts "The line is currently empty."
   else
-    puts "Currently serving #{line[0]}."
-    line.shift
+    sing = "The line is currently:"
+    
+    array.each_with_index do |value, index|
+      sing += "#{index.to_i+1}. #{value}"
+    end
+    puts "#{sing}"
   end
 end
+
+line(katz_deli)
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line"
+  return name, position
+end
+
+take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Grace")
+take_a_number(katz_deli, "Kent")
+
+line(katz_deli)
+
+def now_serving(array)
+  if array.empty? == true
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false
+    puts "Currently serving #{array.shift}"
+  end
+end
+
+now_serving(katz_deli)
